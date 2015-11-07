cda5b254ddb77cfe771db1212ec0ea9c6b8f0d15
killertux <bruno.canguru@hotmail.com>
2015-11-07 17:33:19 -0200

    Added new stuff

---
+++ build/CMakeFiles/MapEditor.dir/DependInfo.cmake
@@ -0,0 +1,18 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/clemente/projects/Troll-Killers/MapEditor.cpp" "/home/clemente/projects/Troll-Killers/build/CMakeFiles/MapEditor.dir/MapEditor.cpp.o"
+  "/home/clemente/projects/Troll-Killers/editorMain.cpp" "/home/clemente/projects/Troll-Killers/build/CMakeFiles/MapEditor.dir/editorMain.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ build/CMakeFiles/MapEditor.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MapEditor.dir/editorMain.cpp.o"
+  "CMakeFiles/MapEditor.dir/MapEditor.cpp.o"
+  "MapEditor.pdb"
+  "MapEditor"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MapEditor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
